210886d137e4385395bd366c98a54916ba78b344
Nanda.GS <nandagopan.gs@cognizant.com>
2017-12-27 00:26:40 +0530

    CORS
    Allow cross origin requests.

--- src/main/java/com/fsd/ServletInitializer.java
+++ src/main/java/com/fsd/ServletInitializer.java
@@ -2,13 +2,29 @@ package com.fsd;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(PmServerApplication.class);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 	
 	
 
