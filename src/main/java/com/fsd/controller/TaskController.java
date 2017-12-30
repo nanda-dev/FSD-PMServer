9ed6acf79bd40a5c4ad8675ab64f5cf00718540a
Nanda.GS <nandagopan.gs@cognizant.com>
2017-12-30 23:56:50 +0530

    Loggind added to task controller

--- src/main/java/com/fsd/controller/TaskController.java
+++ src/main/java/com/fsd/controller/TaskController.java
@@ -2,6 +2,8 @@ package com.fsd.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,36 +19,44 @@ import com.fsd.service.TaskService;
 @CrossOrigin
 public class TaskController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private TaskService taskService;
 	
 	@RequestMapping(value = "/api/task", method = RequestMethod.GET)
 	public List<TaskDTO> getAllTasks() {
+		LOGGER.debug("API Invoked: getAllTasks()");
 		return taskService.getAllTasks();
 	}
 	
 	@RequestMapping(value = "/api/task/project/{id}", method = RequestMethod.GET)
 	public List<TaskDTO> getAllTasksByProject(@PathVariable Long id) {
+		LOGGER.debug("API Invoked: getAllTasksByProject(): " + id);
 		return taskService.getAllTasksByProject(id);
 	}
 	
 	@RequestMapping(value = "/api/task/{id}", method = RequestMethod.GET)
 	public TaskDTO getTaskById(@PathVariable Long id) {
+		LOGGER.debug("API Invoked: getTaskById(): " + id);
 		return taskService.getTaskById(id);
 	}
 	
 	@RequestMapping(value = "/api/task", method = RequestMethod.POST)
 	public TaskDTO addTask(@RequestBody TaskDTO task) {
+		LOGGER.debug("API Invoked: addTask(): " + task.toString());
 		return taskService.addTask(task);
 	}
 	
 	@RequestMapping(value = "/api/task", method = RequestMethod.PUT)
 	public TaskDTO editTask(@RequestBody TaskDTO task) {
+		LOGGER.debug("API Invoked: editTask(): " + task.toString());
 		return taskService.updateTask(task);
 	}
 	
 	@RequestMapping(value = "/api/task/{id}", method = RequestMethod.PUT)
-	public TaskDTO editTask(@PathVariable Long id) {
+	public TaskDTO endTask(@PathVariable Long id) {
+		LOGGER.debug("API Invoked: endTask(): " + id);
 		return taskService.endTask(id);
 	}
 
